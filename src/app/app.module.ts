1d1375af716a3176b9da7b1635d2eddb83aa1c11
Michal <misiek1315@interia.pl>
2021-11-14 22:55:49 +0100

    Chart works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { ChartsModule } from 'ng2-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +23,8 @@ import { DialogBoxComponent } from './dialog-box/dialog-box.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/component/simulation-details/simulation-details.component.ts
+++ src/app/component/simulation-details/simulation-details.component.ts
@@ -1,38 +1,75 @@
 import { Component, OnInit } from '@angular/core';
 import { ControllerService } from 'src/app/services/controller.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Simulation } from 'src/app/models/simulation.model';
 import { SimulationWithStats } from 'src/app/models/simulation-with-stats.model';
+import { ChartDataSets, ChartOptions } from 'chart.js';
+import { Color, Label } from 'ng2-charts';
+import { ChartType } from 'chart.js';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-simulation-details',
   templateUrl: './simulation-details.component.html',
-  styleUrls: ['./simulation-details.component.scss']
+  styleUrls: ['./simulation-details.component.scss'],
 })
 export class SimulationDetailsComponent implements OnInit {
-
   currentSimulation!: SimulationWithStats;
 
+  chartData!: ChartDataModel[];
+
+  public chartLabel: string[] = [];
+
+  lineChartOptions = {
+    responsive: true,
+  };
+
+  lineChartColors: Color[] = [
+    {
+      borderColor: 'black',
+      backgroundColor: 'rgba(255,255,0,0.28)',
+    },
+  ];
+
+  lineChartLegend = true;
+  lineChartPlugins = [];
+  lineChartType: ChartType = 'line';
 
   constructor(
     private controllerService: ControllerService,
     private route: ActivatedRoute,
     private router: Router
-  ) { }
+  ) {}
 
-  ngOnInit(): void {
-    this.getSimulation(this.route.snapshot.params['id']);
+  async ngOnInit(): Promise<void> {
+    var id = this.route.snapshot.params['id'];
+    await this.getSimulation(id);
+    
+    let piModel = new ChartDataModel("pi");
+
+    for (let dailyStats of this.currentSimulation.dailyStatsList) {
+      this.chartLabel.push(dailyStats.day + '');
+      piModel.data.push(dailyStats.pi);
+    }
+    this.chartData = [piModel];
+    // this.chartLabel.push('1', '2', '3', '4', '5', '6', '7', '8', '9', '10');
   }
 
-  getSimulation(id: string): void {
-    this.controllerService.get(id)
-      .subscribe(
-        data => {
-          this.currentSimulation = data;
-          console.log(data);
-        },
-        error => {
-          console.log(error);
-        });
+  async getSimulation(id: string): Promise<void> {
+    try {
+      this.currentSimulation = await lastValueFrom(this.controllerService.get(id));
+      console.log('current simulation data retrieved successfully', this.currentSimulation);
+    } catch (error) {
+      console.error('error retrieving current simulation', error);
+    }
   }
+}
+
+class ChartDataModel {
+  data: number[] = [];
+  label: string;
+ 
+  constructor(label: string) {
+    this.label = label;
+  }
+ 
 }
